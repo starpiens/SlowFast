f4cfd21eee0ccbc47914d41b7ca88310c6a68798
Jihun Kim <starlettkim@gmail.com>
2021-02-22 15:37:24 +0900

    Update ResNet

--- resnet.py
+++ resnet.py
@@ -7,42 +7,52 @@ class ResBlock(nn.Module):
     Single residual block.
     """
 
-    def __init__(self, dim_in, dim_inner, dim_out):
+    def __init__(self,
+                 dim_in,
+                 dim_inner,
+                 dim_out,
+                 temp_kernel_size,
+                 stride):
         super(ResBlock, self).__init__()
         self.dim_in = dim_in
         self.dim_inner = dim_inner
         self.dim_out = dim_out
 
-        # Dimension of the first block in each stage should be 2x.
-        if dim_in != dim_out:
+        # Number of channels and spatial size of input should be matched with output
+        # because of residual connection.
+        if (dim_in != dim_out) or (stride > 1):
             self.conv0 = nn.Conv3d(
                 dim_in,
                 dim_out,
                 kernel_size=(1, 1, 1),
-                stride=(1, 2, 2),
+                stride=(1, stride, stride),
                 bias=False
             )
             self.conv0_bn = nn.BatchNorm3d(dim_out)
 
+        # Tx1x1, dim_inner.
         self.conv1 = nn.Conv3d(
             dim_in,
             dim_inner,
-            kernel_size=(1, 1, 1),
+            kernel_size=(temp_kernel_size, 1, 1),
             bias=False
         )
         self.conv1_bn = nn.BatchNorm3d(dim_inner)
-        self.conv1_act = nn.ReLU(True)
+        self.conv1_act = nn.ReLU(inplace=True)
 
+        # 1x3x3, dim_inner. Stride is applied here.
         self.conv2 = nn.Conv3d(
             dim_inner,
             dim_inner,
-            kernel_size=(3, 3, 3),
+            kernel_size=(1, 3, 3),
+            stride=(1, stride, stride),
+            padding=(0, 1, 1),
             bias=False
         )
         self.conv2_bn = nn.BatchNorm3d(dim_inner)
-        # TODO: Squeeze-and-Excitation module
-        self.conv2_act = nn.ReLU(True)
+        self.conv2_act = nn.ReLU(inplace=True)
 
+        # 1x1x1, dim_out.
         self.conv3 = nn.Conv3d(
             dim_inner,
             dim_out,
@@ -50,7 +60,7 @@ class ResBlock(nn.Module):
             bias=False
         )
         self.conv3_bn = nn.BatchNorm3d(dim_out)
-        self.act = nn.ReLU(True)
+        self.act = nn.ReLU(inplace=True)
 
     def forward(self, x):
         f_x = self.conv1(x)
@@ -62,10 +72,9 @@ class ResBlock(nn.Module):
         f_x = self.conv3(f_x)
         f_x = self.conv3_bn(f_x)
 
-        if self.dim_in == self.dim_out:
-            x = x + f_x
-        else:
-            x = self.conv0_bn(self.conv0(x)) + f_x
+        if hasattr(self, "conv0"):
+            x = self.conv0_bn(self.conv0(x))
+        x = x + f_x
         x = self.act(x)
 
         # TODO: Drop connection
@@ -79,21 +88,30 @@ class ResStage(nn.Module):
                  dim_in,
                  dim_inner,
                  dim_out,
+                 temp_kernel_size,
+                 stride,
                  num_blocks):
         super(ResStage, self).__init__()
-        self.dim_in = dim_in
-        self.dim_out = dim_out
         self.num_blocks = num_blocks
 
-        for i in range(num_blocks):
-            res_block = ResBlock(
-                dim_in if i == 0 else dim_out,
-                dim_inner,
-                dim_out,
-            )
-            self.add_module(f'res_{i}', res_block)
+        for pathway in range(2):
+            for i in range(num_blocks):
+                res_block = ResBlock(
+                    dim_in[pathway] if i == 0 else dim_out[pathway],
+                    dim_inner[pathway],
+                    dim_out[pathway],
+                    temp_kernel_size,
+                    stride if i == 0 else 1
+                )
+                self.add_module(f'res_{i}_{pathway}', res_block)
 
-    def forward(self, x):
-        for child in self.children():
-            x = child(x)
-        return x
+    def forward(self, inputs):
+        output = []
+        for pathway in range(2):
+            x = inputs[pathway]
+            for i in range(self.num_blocks):
+                m = getattr(self, f'res_{i}_{pathway}')
+                x = m(x)
+            output.append(x)
+
+        return output
